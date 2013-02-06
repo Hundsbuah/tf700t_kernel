a0e205288ca3b47a21562289c2777bb10ea28fec
Hundsbuah <christian.lohwasser@gmx.de>
2013-02-06 19:46:00 +0100

    thx _that for the script

--- repack-kernel.sh
+++ repack-kernel.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#thx _that for the script
 ~/Android-Kitchen/tools/mkboot/mkbootfs blob.LNX-ramdisk | gzip > ramdisk.gz
 ~/Android-Kitchen/tools/mkboot/mkbootimg --kernel zImage --ramdisk ramdisk.gz -o blob.LNX
 blobpack blobtmp LNX blob.LNX
