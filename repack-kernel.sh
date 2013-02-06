e9f3a97e7ec6a9917063168f26a98463cf0e5dc0
Hundsbuah <christian.lohwasser@gmx.de>
2013-02-06 19:23:56 +0100

    added repack-kernel.sh shell script

---
+++ repack-kernel.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+~/Android-Kitchen/tools/mkboot/mkbootfs blob.LNX-ramdisk | gzip > ramdisk.gz
+~/Android-Kitchen/tools/mkboot/mkbootimg --kernel zImage --ramdisk ramdisk.gz -o blob.LNX
+blobpack blobtmp LNX blob.LNX
+echo -n "-SIGNED-BY-SIGNBLOB-" > blob
+dd if=/dev/zero bs=1 count=8 >> blob
+cat blobtmp >> blob
