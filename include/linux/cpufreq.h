6a6c8c9505cf681b574875d4372792bf1ffa8488
Hundsbuah <christian.lohwasser@gmx.de>
2013-02-10 23:14:23 +0100

    added pegasusq gov

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -377,6 +377,9 @@ extern struct cpufreq_governor cpufreq_gov_lulzactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)	
 extern struct cpufreq_governor cpufreq_gov_smartass2;	
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartass2)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)	
+extern struct cpufreq_governor cpufreq_gov_pegasusq;	
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_pegasusq)
 #endif
 
 
