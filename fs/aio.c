e7e35daaff00205739197155048bd915b41963f3
Hundsbuah <christian.lohwasser@gmx.de>
2013-02-13 12:46:27 +0100

    revert Don't plug the I/O queue in do_io_submit() changes

--- fs/aio.c
+++ fs/aio.c
@@ -1622,6 +1622,7 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 	struct kioctx *ctx;
 	long ret = 0;
 	int i;
+	struct blk_plug plug;
 
 	if (unlikely(nr < 0))
 		return -EINVAL;
@@ -1638,6 +1639,7 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 		return -EINVAL;
 	}
 
+	blk_start_plug(&plug);
 
 	/*
 	 * AKPM: should this return a partial result if some of the IOs were
@@ -1661,6 +1663,7 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 		if (ret)
 			break;
 	}
+	blk_finish_plug(&plug);
 
 	put_ioctx(ctx);
 	return i ? i : ret;
